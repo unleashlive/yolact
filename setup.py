2306a24a9835e29237a513ee1e3b884ba8bb8dee
michalwisniewski <mw@javi.pl>
2020-02-28 16:58:00 +0100

    feature: Add setup.py script

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='yolact',
+    version='1.0.0',
+    url='http://https://github.com/unleashlive/yolact/',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
